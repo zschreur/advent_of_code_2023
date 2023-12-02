0bf070bd76567837bbff9cf166cc941bde5980ca
Zackary Schreur <code@zackaryschreur.com>
2023-12-02 07:23:57 -0500

    Setup for multiple days

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,19 @@ pub mod setup {
         let res = std::fs::read_to_string(&path)?;
         Ok(res)
     }
+
+    pub struct Args {
+        pub day: usize,
+        pub puzzle_input: String,
+    }
+    
+    pub fn parse_args(args: &[String]) -> Result<Args, Box<dyn std::error::Error>> {
+        let day = args[1].clone().parse::<usize>()?;
+        let file_path = args[2].clone();
+
+        let puzzle_input = read_file(&file_path)?;
+        Ok(Args{day, puzzle_input})
+    }
 }
 
 pub mod day_one;
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
-use advent_of_code_2023::setup::read_file;
 use advent_of_code_2023::day_one;
+use advent_of_code_2023::setup::*;
 
 fn main() {
-    // exec puzzle_input_path
     let args: Vec<String> = std::env::args().collect();
-    let puzzle_input_path = args.get(1).unwrap();
-    match read_file(&puzzle_input_path) {
-        Ok(input) => {
-            day_one::run(&input);
+    match parse_args(&args) {
+        Ok(Args{day,  puzzle_input}) if day == 1 => {
+            day_one::run(&puzzle_input);
         },
-        Err(e) => {
-            eprint!("{}", e);
-        }
+        Err(e) => eprintln!("{}", e),
+        Ok(a) => eprintln!("Day not implemented: {}", a.day)
     }
 }
