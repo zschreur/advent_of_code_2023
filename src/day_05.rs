07af5b0570ad95b01382bf2e63741607d012c234
Zackary Schreur <51675520+zschreur@users.noreply.github.com>
2023-12-06 11:28:43 -0500

    Day 5 Part 1

--- src/day_05.rs
+++ src/day_05.rs
@@ -1,6 +1,6 @@
-use std::collections::HashMap;
 use std::str::FromStr;
 
+#[derive(Debug)]
 enum Element {
     Seed,
     Soil,
@@ -36,9 +36,9 @@ impl FromStr for Element {
 #[derive(Debug)]
 struct ParsingMappingError;
 
+#[derive(Debug)]
 struct Mapping {
     source: Element,
-    destination: Element,
 }
 
 impl FromStr for Mapping {
@@ -55,15 +55,13 @@ impl FromStr for Mapping {
         };
 
         match get_elements(&s) {
-            Some(a) => Ok(Mapping {
-                source: a.0,
-                destination: a.1,
-            }),
+            Some(a) => Ok(Mapping { source: a.0 }),
             None => Err(ParsingMappingError),
         }
     }
 }
 
+#[derive(Debug, Clone, Copy)]
 struct Rule {
     destination_start: usize,
     source_start: usize,
@@ -76,6 +74,20 @@ impl Rule {
             destination_start,
             source_start,
             length,
+        }
+    }
+
+    fn apply(&self, source_number: usize) -> Option<usize> {
+        if source_number >= self.source_start && source_number < self.source_start + self.length {
+            if self.source_start < self.destination_start {
+                Some(source_number + (self.destination_start - self.source_start))
+            } else if self.source_start > self.destination_start {
+                Some(source_number - (self.source_start - self.destination_start))
+            } else {
+                Some(source_number)
+            }
+        } else {
+            None
         }
     }
 }
@@ -99,21 +111,77 @@ impl FromStr for Rule {
         };
 
         match get_range(&s) {
-            Some((destination_start, source_start, length)) => Ok(Rule::new(destination_start, source_start, length)),
-            _ => Err(ParseRuleError)
-
+            Some((destination_start, source_start, length)) => {
+                Ok(Rule::new(destination_start, source_start, length))
+            }
+            _ => Err(ParseRuleError),
         }
     }
 }
 
-pub struct Puzzle;
+#[derive(Debug)]
+struct Almanac([Vec<Rule>; 7]);
+
+impl Almanac {
+    fn new() -> Self {
+        Almanac(std::array::from_fn(|_| Vec::new()))
+    }
+
+    fn set_rule(&mut self, source: &Element, rule: &Rule) {
+        let element_index = match source {
+            Element::Seed => 0,
+            Element::Soil => 1,
+            Element::Fertilizer => 2,
+            Element::Water => 3,
+            Element::Light => 4,
+            Element::Temperature => 5,
+            Element::Humidity => 6,
+            Element::Location => panic!("Location as source is not expected"),
+        };
+
+        self.0[element_index].push(*rule);
+    }
+
+    fn seed_info(&self, seed_number: usize) -> [usize; 7] {
+        let mut res = [0; 7];
+
+        let mut current_element_number = seed_number;
+        for element_index in 0..7 {
+            let rules = &self.0[element_index];
+            let n = match rules
+                .iter()
+                .find_map(|rule| rule.apply(current_element_number))
+            {
+                Some(n) => n,
+                _ => current_element_number,
+            };
+
+            current_element_number = n;
+            res[element_index] = n;
+        }
+
+        res
+    }
+}
+
+#[derive(Debug)]
+pub struct Puzzle {
+    almanac: Almanac,
+    seeds: Vec<usize>,
+}
 
 impl Puzzle {
     pub fn create(input: String) -> Box<dyn super::Puzzle> {
-        Box::new(Self)
+        Box::new(Self::new(&input))
     }
 
-    fn parse_input(input: &str) -> () {
+    fn new(input: &str) -> Self {
+        let (almanac, seeds) = Self::parse_input(&input);
+
+        Self { almanac, seeds }
+    }
+
+    fn parse_input(input: &str) -> (Almanac, Vec<usize>) {
         let mut lines = input.lines();
         let seeds = lines
             .next()
@@ -126,20 +194,35 @@ impl Puzzle {
             .unwrap_or(vec![]);
         let lines = lines.skip(1);
 
-        lines.fold(0, |acc, line| {
-            if let Ok(m) = line.parse::<Mapping>() {
-            } else if let Ok(rule) = line.parse::<Rule>() {
-            } else {
-            }
+        let a = lines.fold(
+            (Almanac::new(), Element::Seed),
+            |(mut almanac, source), line| {
+                if let Ok(m) = line.parse::<Mapping>() {
+                    (almanac, m.source)
+                } else {
+                    match line.parse::<Rule>() {
+                        Ok(rule) => almanac.set_rule(&source, &rule),
+                        _ => (),
+                    }
+                    (almanac, source)
+                }
+            },
+        );
 
-            acc
-        });
+        (a.0, seeds)
     }
 }
 
 impl super::Puzzle for Puzzle {
     fn run_part_one(&self) {
-        println!("Part 1: {}", "NOT IMPLEMENTED");
+        let almanac = &self.almanac;
+        let seeds = &self.seeds;
+        let all_seed_info = seeds
+            .iter()
+            .map(|s| almanac.seed_info(*s)[6])
+            .collect::<Vec<usize>>();
+        let min_location = *all_seed_info.iter().min().unwrap();
+        println!("Part 1: {}", min_location);
     }
     fn run_part_two(&self) {
         println!("Part 2: {}", "NOT IMPLEMENTED");
@@ -186,12 +269,15 @@ humidity-to-location map:
 
     #[test]
     fn parse_input_test() {
-        // Puzzle::parse_input(SAMPLE_INPUT);
-        assert!(false);
+        let (almanac, seeds) = Puzzle::parse_input(&SAMPLE_INPUT);
+        let all_seed_info = seeds
+            .iter()
+            .map(|s| almanac.seed_info(*s))
+            .collect::<Vec<[usize; 7]>>();
+        let min_location = all_seed_info.iter().min_by(|x, y| x[6].cmp(&y[6])).unwrap();
+        assert_eq!(min_location[6], 35);
     }
 
     #[test]
-    fn take_while_test() {
-        let input = "seed-to-soil map:\n50 98 2\n52 50 48\n\nsoil-to-fertilizer map:\n0 15 37\n37 52 2\n39 0 15";
-    }
+    fn take_while_test() {}
 }
