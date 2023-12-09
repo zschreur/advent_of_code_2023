cba9a0b6948b259607c29db09ede7469fc8d898a
Zackary Schreur <51675520+zschreur@users.noreply.github.com>
2023-12-09 07:26:07 -0500

    Day 9 Part 1

--- src/day_09.rs
+++ src/day_09.rs
@@ -1,10 +1,19 @@
-pub struct Puzzle;
+use std::collections::{HashSet};
 
-struct OasisHistory()
+pub struct Puzzle(Vec<Vec<i32>>);
 
 impl Puzzle {
     fn new(input: &str) -> Self {
-        Self
+        let sequences = input
+            .lines()
+            .map(|line| {
+                line.split_whitespace()
+                    .filter_map(|num| num.parse::<i32>().ok())
+                    .collect::<Vec<i32>>()
+            })
+            .collect::<Vec<Vec<i32>>>();
+
+        Self(sequences)
     }
 
     pub fn create(input: String) -> Box<dyn super::Puzzle> {
@@ -13,17 +22,54 @@ impl Puzzle {
 }
 
 impl super::Puzzle for Puzzle {
-    fn run_part_one(&self) {}
+    fn run_part_one(&self) {
+        let res = self
+            .0
+            .iter()
+            .map(|seq| find_next_value(&seq))
+            .reduce(|acc, v| acc + v);
+        println!("Part 1: {}", res.unwrap());
+    }
 
     fn run_part_two(&self) {}
+}
+
+fn find_next_value(sequence: &Vec<i32>) -> i32 {
+    let d = sequence
+        .windows(2)
+        .map(|w| &w[1] - &w[0])
+        .collect::<Vec<i32>>();
+    let s = HashSet::<&i32>::from_iter(d.iter());
+
+    sequence.last().unwrap()
+        + if s.len() == 1 {
+            d[0]
+        } else {
+            find_next_value(&d)
+        }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const SAMPLE_INPUT: &str = "";
+    const SAMPLE_INPUT: &str = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
 
     #[test]
-    fn test() {}
+    fn test_parse_input() {
+        let p = Puzzle::new(&SAMPLE_INPUT);
+        assert_eq!(p.0.len(), 3);
+        assert_eq!(p.0[0], vec![0, 3, 6, 9, 12, 15]);
+        assert_eq!(p.0[1], vec![1, 3, 6, 10, 15, 21]);
+        assert_eq!(p.0[2], vec![10, 13, 16, 21, 30, 45]);
+    }
+
+    #[test]
+    fn test_find_next_value() {
+        assert_eq!(find_next_value(&vec![0, 3, 6, 9, 12, 15]), 18);
+        assert_eq!(find_next_value(&vec![1, 3, 6, 10, 15, 21]), 28);
+        assert_eq!(find_next_value(&vec![10, 13, 16, 21, 30, 45]), 68);
+    }
 }
