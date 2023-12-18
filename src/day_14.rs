3ff608908a0c15e5f31e9c356a309f34eaddc364
Zackary Schreur <z.schreur@techsmith.com>
2023-12-18 14:20:29 -0500

    Day 14 Part 2

--- src/day_14.rs
+++ src/day_14.rs
@@ -1,9 +1,17 @@
 use std::fmt;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Rock {
     Round,
     Cube,
+}
+
+#[derive(PartialEq)]
+enum Direction {
+    North,
+    South,
+    East,
+    West,
 }
 
 pub struct Platform {
@@ -56,36 +64,71 @@ impl Platform {
         Self::new(rocks, size)
     }
 
-    fn tilt(&mut self) -> () {
+    fn tilt(&mut self, direction: Direction) -> () {
         let mut v = vec![None; self.size * self.size];
-        for x in 0..self.size {
-            self.rocks
+        for i in 0..self.size {
+            let mut list = match direction {
+                Direction::North | Direction::South => self
+                    .rocks
+                    .iter()
+                    .skip(i)
+                    .step_by(self.size)
+                    .collect::<Vec<_>>(),
+                Direction::West | Direction::East => self
+                    .rocks
+                    .iter()
+                    .skip(i * self.size)
+                    .take(self.size)
+                    .collect::<Vec<_>>(),
+            };
+            if direction == Direction::South || direction == Direction::East {
+                list.reverse();
+            }
+
+            let mut last_insert_pos = 0;
+            let mut foo = list
                 .iter()
-                .skip(x)
-                .step_by(self.size)
-                .collect::<Vec<_>>()
-                .split_inclusive_mut(|r| match r {
-                    Some(Rock::Cube) => true,
-                    _ => false,
-                })
-                .map(|slice| {
-                    slice.sort_by(|a, b| match (a, b) {
-                        (Some(Rock::Round), None) => std::cmp::Ordering::Less,
-                        (None, Some(Rock::Round)) => std::cmp::Ordering::Greater,
-                        (Some(Rock::Cube), _) => std::cmp::Ordering::Greater,
-                        (_, Some(Rock::Cube)) => std::cmp::Ordering::Less,
-                        _ => std::cmp::Ordering::Equal,
-                    });
-                    slice
-                })
-                .flatten()
                 .enumerate()
-                .for_each(|(index, r)| {
-                    v[index * self.size + x] = r.clone();
+                .fold(vec![None; list.len()], |mut acc, a| {
+                    match a.1 {
+                        Some(Rock::Round) => {
+                            acc[last_insert_pos] = Some(Rock::Round);
+                            last_insert_pos += 1;
+                        }
+                        Some(Rock::Cube) => {
+                            acc[a.0] = Some(Rock::Cube);
+                            last_insert_pos = a.0 + 1;
+                        }
+                        _ => (),
+                    };
+
+                    acc
                 });
+
+            if direction == Direction::South || direction == Direction::East {
+                foo.reverse();
+            }
+
+            match direction {
+                Direction::North | Direction::South => {
+                    foo.iter().enumerate().for_each(|(index, r)| {
+                        v[index * self.size + i] = r.clone();
+                    });
+                }
+                Direction::West | Direction::East => {
+                    v.splice((i * self.size)..((i + 1) * self.size), foo);
+                }
+            };
         }
 
         self.rocks = v;
+    }
+
+    fn cycle(&mut self) {
+        self.tilt(Direction::North);
+        self.tilt(Direction::West);
+        self.tilt(Direction::South);
+        self.tilt(Direction::East);
     }
 
     fn calculate_load(&self) -> usize {
@@ -115,13 +158,43 @@ impl Puzzle {
 impl super::Puzzle for Puzzle {
     fn run_part_one(&self) {
         let mut platform = Platform::from_input(&self.0);
-        platform.tilt();
+        platform.tilt(Direction::North);
 
         let load = platform.calculate_load();
         println!("Part 1: {}", load);
     }
 
-    fn run_part_two(&self) {}
+    fn run_part_two(&self) {
+        let mut cache: std::collections::HashMap<_, usize> = std::collections::HashMap::new();
+        let mut platform = Platform::from_input(&self.0);
+        cache.insert(platform.rocks.clone(), 0);
+        let mut res: Option<usize> = None;
+        loop {
+            platform.cycle();
+            let cycle_number = cache.len();
+
+            if let Some(m) = cache.get(&platform.rocks) {
+                let k = cycle_number - m;
+                let goal = ((1_000_000_000 - m) % k) + m;
+                if let Some(v_n) = cache.iter().find(|(_, value)| *value == &goal) {
+                    let goal = Platform {
+                        rocks: v_n.0.to_vec(),
+                        size: platform.size,
+                    };
+                    res = Some(goal.calculate_load());
+                }
+
+                break;
+            } else {
+                cache.insert(platform.rocks.clone(), cycle_number);
+            }
+
+            if cycle_number == 1_000_000_000 {
+                break;
+            }
+        }
+        println!("Part 2: {}", res.expect("No result found"));
+    }
 }
 
 #[cfg(test)]
@@ -149,11 +222,41 @@ O.#..O.#.#
     #[test]
     fn test_tilt() {
         let mut platform = Platform::from_input(SAMPLE_INPUT);
-
-        println!("{}", platform);
-        platform.tilt();
-        println!("\n{}", platform);
+        platform.tilt(Direction::North);
 
         assert_eq!(platform.calculate_load(), 136);
+    }
+
+    #[test]
+    fn test_cycle() {
+        let mut cache: std::collections::HashMap<_, usize> = std::collections::HashMap::new();
+        let mut platform = Platform::from_input(SAMPLE_INPUT);
+        cache.insert(platform.rocks.clone(), 0);
+        let mut res: Option<usize> = None;
+        loop {
+            platform.cycle();
+            let cycle_number = cache.len();
+
+            if let Some(m) = cache.get(&platform.rocks) {
+                let k = cycle_number - m;
+                let goal = ((1_000_000_000 - m) % k) + m;
+                if let Some(v_n) = cache.iter().find(|(_, value)| *value == &goal) {
+                    let goal = Platform {
+                        rocks: v_n.0.to_vec(),
+                        size: platform.size,
+                    };
+                    res = Some(goal.calculate_load());
+                }
+
+                break;
+            } else {
+                cache.insert(platform.rocks.clone(), cycle_number);
+            }
+
+            if cycle_number == 1_000_000_000 {
+                break;
+            }
+        }
+        assert_eq!(res.unwrap(), 64);
     }
 }
