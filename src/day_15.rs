f1a81927dc1fe0ecba19e802f572147018dbfb9e
Zackary Schreur <z.schreur@techsmith.com>
2023-12-18 17:08:57 -0500

    Day 15 Part 1

--- src/day_15.rs
+++ src/day_15.rs
@@ -1,8 +1,17 @@
+fn hash_step(s: &str) -> u128 {
+    s.chars().fold(0, |acc, x| ((acc + x as u128) * 17) % 256)
+}
+
+fn run_initialization_sequence(seq: Vec<&str>) -> u128 {
+    seq.iter().fold(0, |acc, x| acc + hash_step(x))
+}
+
 pub struct Puzzle(String);
 
 impl Puzzle {
     fn new(input: &str) -> Self {
-        Self(input.to_string())
+        let seq = input.replace("\n", "");
+        Self(seq)
     }
 
     pub fn create(input: String) -> Box<dyn super::Puzzle> {
@@ -11,13 +20,24 @@ impl Puzzle {
 }
 
 impl super::Puzzle for Puzzle {
-    fn run_part_one(&self) {}
+    fn run_part_one(&self) {
+        let res = run_initialization_sequence(self.0.split(",").collect());
+        println!("Part 1: {}", res);
+    }
 
     fn run_part_two(&self) {}
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    const INPUT: &str = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
+
     #[test]
-    fn test() {}
+    fn test_sample() {
+        assert_eq!(
+            run_initialization_sequence(INPUT.split(",").collect()),
+            1320
+        );
+    }
 }
